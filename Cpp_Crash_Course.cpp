644273ac9c23ddf1de0d120b8949fbf68efa74ab
aykutmayali <aykutmayali@gmail.com>
2020-10-10 23:10:12 +0300

    first commit

---
+++ Cpp_Crash_Course.cpp
@@ -0,0 +1,22 @@
+// Cpp_Crash_Course.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int age = 25;
+    age = 26;
+    float weight = 75;
+    double balance = 6776.67;
+    char gender = 'm';
+    string username = "mayali";
+    bool isTodaySunny = false;
+
+    std::cout << "Hello World!\n";
+    system("pause>0");
+
+}
+
