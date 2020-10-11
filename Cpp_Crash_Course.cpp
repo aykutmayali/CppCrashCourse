78d071f2cedb1c7cc3a3c9da6baba8e983940d4b
aykutmayali <aykutmayali@gmail.com>
2020-10-11 14:59:19 +0300

    decision loops

--- Cpp_Crash_Course.cpp
+++ Cpp_Crash_Course.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    //kind of variables
+    // ---------------- kind of variables --------- //
     int age = 25;
     age = 26;
     float weight = 75;
@@ -15,18 +15,34 @@ int main()
     char gender = 'm';
     string username = "mayali";
     bool isTodaySunny = false;
+    bool isTodayWeekend = false;
     string colors[10] = { "red","green","pink","yellow","blue" };
  //   cout << colors[0];
  //   cin >> colors[5];
  //   cout << colors[5];
- //   std::cout << "Hello World!\n";
-    if (isTodaySunny) {
+ //   std::cout << "Hello World!\n"; //this std symbol of namespace , and if you use it upper side not need to write here again
+
+
+    // ------------------ decision loops ----------- //
+   /* if (isTodaySunny) {
         cout << "go to the park" << endl;
     }
     else {
         cout << "take an umbrella" << endl;
+    }*/
+
+   // isTodaySunny ? cout << "Go to park" : cout << "Take an umbrella";
+
+    if (isTodayWeekend && isTodaySunny)
+    {
+        cout << "Go to the park, it is sunny" <<endl;
     }
-    isTodaySunny ? cout << "Go to park" : cout << "Take an umbrella";
+    else if (isTodayWeekend && !isTodaySunny) {
+        cout << "Go to the park , but take an umbrella" << endl;
+    }
+    else {
+        cout << "Go to work anyway" << endl;
+    }
     
     
     system("pause>0");
