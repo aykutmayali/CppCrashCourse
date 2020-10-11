4073c61ee65f08bbaca7307b6f8a82a8b79f20d8
aykutmayali <aykutmayali@gmail.com>
2020-10-11 15:25:45 +0300

    switch case

--- Cpp_Crash_Course.cpp
+++ Cpp_Crash_Course.cpp
@@ -5,25 +5,31 @@
 #include <string>
 using namespace std;
 
+
+
 int main()
 {
-    // ---------------- kind of variables --------- //
-    int age = 25;
+    // ***************---------------- kind of variables ---------*************** //
+    /*int age = 25;
     age = 26;
     float weight = 75;
     double balance = 6776.67;
     char gender = 'm';
-    string username = "mayali";
+    string username = "mayali ";
     bool isTodaySunny = false;
     bool isTodayWeekend = false;
-    string colors[10] = { "red","green","pink","yellow","blue" };
+    string colors[10] = { "red","green","pink","yellow","blue" };*/
+ //   cout << balance;
+ //   cout << username;
  //   cout << colors[0];
  //   cin >> colors[5];
  //   cout << colors[5];
  //   std::cout << "Hello World!\n"; //this std symbol of namespace , and if you use it upper side not need to write here again
 
 
-    // ------------------ decision loops ----------- //
+    // ***************------------------ decision & loops -----------*************** //
+
+    // if-else mechanism
    /* if (isTodaySunny) {
         cout << "go to the park" << endl;
     }
@@ -33,7 +39,7 @@ int main()
 
    // isTodaySunny ? cout << "Go to park" : cout << "Take an umbrella";
 
-    if (isTodayWeekend && isTodaySunny)
+   /* if (isTodayWeekend && isTodaySunny)
     {
         cout << "Go to the park, it is sunny" <<endl;
     }
@@ -42,8 +48,24 @@ int main()
     }
     else {
         cout << "Go to work anyway" << endl;
-    }
+    }*/
     
+
+    // switch case mechanism
+    //enum EyeColor { Brown, Blue, Green, Gray, Other }; // think them like an integer 0,1,2,3,4
+
+    //EyeColor eyeColor = Brown;
+    //int rand = 9;
+    //switch (eyeColor) { // switch looks for int , switch (rand) turns default
+    //case Brown: cout << "Most of people have Brown eyes"; break;
+    //case Blue: cout << "10% of  people have Blue eyes ";  break;
+    //case Green: cout << "2% of  people have Green eyes ";  break;
+    //case Gray: cout << "1% of  people have Gray eyes ";   break;
+    //case Other: cout << "1% of  people have other color eyes ";   break;
+    //default: cout << "Not a valid eye color";
+    //}
+
+
     
     system("pause>0");
 
