b86aced693d46ff4d2c32eff6e793e7f66d917ab
aykutmayali <aykutmayali@gmail.com>
2020-10-11 18:44:22 +0300

    functions

--- Cpp_Crash_Course.cpp
+++ Cpp_Crash_Course.cpp
@@ -5,11 +5,15 @@
 #include <string>
 using namespace std;
 
-
+// ------------function declaration
+double sum(double a, double b); // declare before main fuction
+void introduceMe(string name, int age=0);
+void celebrateBirthday(int age);
 
 int main()
 {
     // ***************---------------- kind of variables ---------*************** //
+    // variable is a container stores value 
     /*int age = 25;
     age = 26;
     float weight = 75;
@@ -19,27 +23,23 @@ int main()
     bool isTodaySunny = false;
     bool isTodayWeekend = false;
     string colors[10] = { "red","green","pink","yellow","blue" };*/
- //   cout << balance;
- //   cout << username;
- //   cout << colors[0];
- //   cin >> colors[5];
- //   cout << colors[5];
- //   std::cout << "Hello World!\n"; //this std symbol of namespace , and if you use it upper side not need to write here again
-
+    //   cout << balance;
+     //   cout << username;
+     //   cout << colors[0];
+     //   cin >> colors[5];
+     //   cout << colors[5];
+     //   std::cout << "Hello World!\n"; //this std symbol of namespace , and if you use it upper side not need to write here again
 
     // ***************------------------ decision & loops -----------*************** //
-
     // ---------if-else mechanism
-   /* if (isTodaySunny) {
+    /* if (isTodaySunny) {
         cout << "go to the park" << endl;
     }
     else {
         cout << "take an umbrella" << endl;
     }*/
-
-   // isTodaySunny ? cout << "Go to park" : cout << "Take an umbrella";
-
-   /* if (isTodayWeekend && isTodaySunny)
+    // isTodaySunny ? cout << "Go to park" : cout << "Take an umbrella";
+    /* if (isTodayWeekend && isTodaySunny)
     {
         cout << "Go to the park, it is sunny" <<endl;
     }
@@ -49,11 +49,8 @@ int main()
     else {
         cout << "Go to work anyway" << endl;
     }*/
-    
-
     // ---------switch case mechanism
     //enum EyeColor { Brown, Blue, Green, Gray, Other }; // think them like an integer 0,1,2,3,4
-
     //EyeColor eyeColor = Brown;
     //int rand = 9;
     //switch (eyeColor) { // switch looks for int , switch (rand) turns default
@@ -64,7 +61,6 @@ int main()
     //case Other: cout << "1% of  people have other color eyes ";   break;
     //default: cout << "Not a valid eye color";
     //}
-
     // ---------while loops
     /*int counter = 1;
     while (counter <= 10) {
@@ -77,9 +73,8 @@ int main()
         cout << counter << endl;
         counter++;
     } while (counter <= 10);*/
-
     // ------------for loops
-    string animals[5] = { "cat","dog","cow","goat","bee" };
+    /*string animals[5] = { "cat","dog","cow","goat","bee" };
     int sizeofArray = sizeof(animals);
     int sizeofElement = sizeof(animals[0]);
     int lengthofArray = sizeofArray / sizeofElement;       
@@ -87,8 +82,20 @@ int main()
     for (int i = 0; i < lengthofArray; i++)
     {
         cout << animals[i] << endl;
-    }
-         
+    }*/
+
+    // ***************------------------ functions -----------*************** //
+    /* cout << sum(2.2, 7.7) << endl;
+    introduceMe("kerem", 25);
+    introduceMe("aykut");*/
+
+    // ***************------------------ pointers -----------*************** //
+    // pointers stores an address of an variable
+    // using pointers with functions
+    int myAge = 25;
+    cout << "Before Function, age  "<< myAge << endl;
+    celebrateBirthday(myAge);
+    cout << "After Function, age  " << myAge << endl;
 
 
     
@@ -96,3 +103,17 @@ int main()
 
 }
 
+// ------------function definition
+double sum(double a, double b) {  
+    return a + b;
+}
+
+void introduceMe(string name, int age) {
+    cout << "My name is " << name << endl;
+    cout << "I am " << age << " years old" << endl;
+}
+
+void celebrateBirthday(int age) {
+    age++;
+    cout << " Celebrated " << age << ". birthday" << endl;
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # CppCrashCourse
 Cpp with Visual Studio
+
+Main Topics shown with // *** --- ***  //
+
+Subtopics shown with // ---
+
+Blocks shown with /* ... */
+
+>for take in comment use Ctrl + K + C
+
+>for take in uncomment use Ctrl + K + U
+
+>for Build use Ctrl + B
+
+>for Running use F5
+
+#C++
+#Visual Studio
+
