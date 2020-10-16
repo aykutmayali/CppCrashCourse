6e2cbb1c4497bfeafe1a8c5bd46e081796a5419c
aykutmayali <aykutmayali@gmail.com>
2020-10-17 00:04:24 +0300

    sametree class

---
+++ sameTree.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	
+};
+
+
+class Solution {
+public:
+	bool isSameTree(TreeNode* a, TreeNode* b) {
+		if (a == NULL && b == NULL) return true;
+		if (a != NULL && b == NULL) return false;
+		if (a == NULL && b != NULL) return false;
+		if (a->val == b->val && isSameTree(a->left, b->left) && isSameTree(a->right, b->right)) {
+			return true;
+		}
+		return false;
+	}
+
+};
