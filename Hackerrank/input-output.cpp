cf2398f8d5aafaf402a55f0b61627b7c8e79cb4b
aykutmayali <aykutmayali@gmail.com>
2020-10-23 09:50:47 +0300

    hackerrank-files-added

---
+++ Hackerrank/input-output.cpp
@@ -0,0 +1,26 @@
+//string s;
+//int n;
+//
+//cin >> s >> n;
+//cout << s << " " << n << endl;
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a;
+    int b;
+    int c;
+    int d;
+    cin >> a >> b >> c;
+    d = a + b + c;
+    cout << d;
+
+    return 0;
+}
