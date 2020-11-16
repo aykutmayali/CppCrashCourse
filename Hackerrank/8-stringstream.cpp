ee702b9b61dc514b487f5f7032fda46cda42a670
aykutmayali <aykutmayali@gmail.com>
2020-11-16 17:52:01 +0300

    Update 8-stringstream.cpp

--- Hackerrank/8-stringstream.cpp
+++ Hackerrank/8-stringstream.cpp
@@ -4,24 +4,26 @@
 using namespace std;
 
 vector<int> parseInts(string str) {
-    // Complete this function   
+	// Complete this function   
     vector<int> rr;
-    char ch;
-    //rr.resize(str.size());
-    for (int i = 0;i < str.size();i++) {
-        if (str[i] != ',') {
-            rr.push_back(str[i]);
-            //cout << str[i] << "\n";
-        }
-        else {
-            ch = str[i];
+    string temp;
+    stringstream ss;
+    int num;
+    // cout<<str.size(); length = 7 / 2 3 , 4 , 5 6
+    for(int i=0; i<str.size(); ++i){
+        if(str[i] != ','){ // 2 3 , 4 ,
+            temp = temp + str[i]; // 2 3 
+            cout<<temp<<" "<<" as string in if"<<" i: "<<i <<"\n";
+        } else{
+           cout<<temp<<" "<<" as string in else"<<" i: "<<i <<"\n";           
+           ss<<temp; // ss<-23 temp           
+           ss>>num;  //23->num
+           cout<<num<<" "<<"as number"<<" i: "<<i <<"\n";          
+           rr.push_back(num); //  rr<-- 23
+           temp="";  // temp = ""
+           
         }
     }
-    cout << rr[0] << "\n";
-    cout << rr[1] << "\n";
-    cout << rr[2] << "\n";
-    cout << rr[3] << "\n";
-    cout << ch << "\n";
     return rr;
 }
 
@@ -29,10 +31,10 @@ int main() {
     string str;
     cin >> str;
     vector<int> integers = parseInts(str);
-    for (int i = 0; i < integers.size(); i++) {
+    for(int i = 0; i < integers.size(); i++) {
         cout << integers[i] << "\n";
     }
-
-
+    
+    
     return 0;
 }
