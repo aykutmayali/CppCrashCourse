c3ab5a255728e36e91c48b78fe9e34cd2e615fc7
aykutmayali <aykutmayali@gmail.com>
2020-11-19 10:28:49 +0300

    stringstream

--- Hackerrank/8-stringstream.cpp
+++ Hackerrank/8-stringstream.cpp
@@ -6,22 +6,33 @@ using namespace std;
 vector<int> parseInts(string str) {
     // Complete this function   
     vector<int> rr;
-    char ch;
-    //rr.resize(str.size());
-    for (int i = 0;i < str.size();i++) {
-        if (str[i] != ',') {
-            rr.push_back(str[i]);
-            //cout << str[i] << "\n";
+    string temp;
+    stringstream ss;
+    int num;
+    // cout<<str.size(); length = 7 / 2 3 , 4 , 5 6
+    for (int i = 0; i < str.size(); ++i) {
+        if (str[i] != ',') { // 2 3 , 4 ,
+            temp = temp + str[i]; // 2 3 
+           // cout<<temp<<" "<<" as string in if"<<" i: "<<i <<"\n";
+            if (i == str.size() - 1) {
+                // cout<<temp<<" "<<" as string in else"<<" i: "<<i <<"\n";           
+                ss << temp; // ss<-23 temp           
+                ss >> num;  //23->num
+               // cout<<num<<" "<<"as number"<<" i: "<<i <<"\n";          
+                rr.push_back(num);
+            }
         }
         else {
-            ch = str[i];
+
+            //cout<<temp<<" "<<" as string in else"<<" i: "<<i <<"\n";           
+            ss << temp; // ss<-23 temp           
+            ss >> num;  //23->num
+           // cout<<num<<" "<<"as number"<<" i: "<<i <<"\n";          
+            rr.push_back(num); //  rr<-- 23
+            temp = "";  // temp = ""
+            ss.clear();
         }
     }
-    cout << rr[0] << "\n";
-    cout << rr[1] << "\n";
-    cout << rr[2] << "\n";
-    cout << rr[3] << "\n";
-    cout << ch << "\n";
     return rr;
 }
 
